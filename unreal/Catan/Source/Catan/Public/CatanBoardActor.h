816b37b37afd0a0aab536476af0e419629eedd63
Vadim <regoni@ya.ru>
2026-08-12 19:11:30 +0300

    feat: add procedural board art and game pieces

--- unreal/Catan/Source/Catan/Public/CatanBoardActor.h
+++ unreal/Catan/Source/Catan/Public/CatanBoardActor.h
@@ -39,16 +39,47 @@ private:
     TArray<TObjectPtr<UStaticMeshComponent>> RoadSlots;
 
     UPROPERTY(Transient)
+    TArray<TObjectPtr<UStaticMeshComponent>> BuildingBodies;
+
+    UPROPERTY(Transient)
+    TArray<TObjectPtr<UStaticMeshComponent>> BuildingRoofs;
+
+    UPROPERTY(Transient)
+    TArray<TObjectPtr<UStaticMeshComponent>> TokenSlots;
+
+    UPROPERTY(Transient)
+    TArray<TObjectPtr<UStaticMeshComponent>> Decorations;
+
+    TArray<uint8> RenderedHexResources;
+    int32 ResourceGeneration = 0;
+
+    UPROPERTY(Transient)
     TArray<TObjectPtr<UTextRenderComponent>> Labels;
+
+    UPROPERTY(Transient)
+    TArray<TObjectPtr<UTextRenderComponent>> PortLabels;
+
+    UPROPERTY(Transient)
+    TObjectPtr<UStaticMeshComponent> RobberPiece;
+
+    UPROPERTY(Transient)
+    TObjectPtr<UStaticMeshComponent> RobberTop;
 
     UPROPERTY(Transient)
     TObjectPtr<UMaterialInterface> BasicMaterial;
 
     void BuildBoard();
+    void BuildEnvironment();
     void BuildHexes();
+    void CreateHexSection(int32 Index, const FVector& Center, const FLinearColor& Color);
+    void BuildResourceDecorations();
+    void BuildPorts();
     void BuildHexHitTargets();
     void BuildNodes();
     void BuildRoads();
+    UStaticMeshComponent* AddDecoration(const FString& Name, UStaticMesh* Mesh,
+        const FVector& Location, const FVector& Scale, const FLinearColor& Color,
+        const FRotator& Rotation = FRotator::ZeroRotator);
 
     UFUNCTION()
     void RefreshPieces();
